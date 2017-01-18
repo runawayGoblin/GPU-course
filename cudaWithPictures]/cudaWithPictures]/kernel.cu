2d1d430ae392e49a1ee916da9f7fbbcb2b182787
Morgan Young <myoung@carthage.edu>
2017-01-18 11:56:38 -0600

    added blur function and timer
    Added a box fliter function and a timer to time how long if took to run
    on the CPU

--- cudaWithPictures]/cudaWithPictures]/kernel.cu
+++ cudaWithPictures]/cudaWithPictures]/kernel.cu
@@ -4,10 +4,15 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include<iostream>
+#include"HighPerfTimer.h"
+
+
 
 
 using namespace cv;
 using namespace std;
+
+typedef unsigned char UBYTE;
 
 void CpuThreshold(int threshold, int width, int height, unsigned char* data);
 cudaError_t mallocGPU(unsigned char** orig, unsigned char** modif, int width, int height);
@@ -15,6 +20,9 @@ void cleanGPU(unsigned char* orig, unsigned char*);
 cudaError_t copyToGPU(unsigned char* gpuData, unsigned char* cpuData, int width, int height);
 cudaError_t copyToCPU(unsigned char* gpuData, unsigned char* cpuData, int width, int height);
 void onTrack(int thr, void* pt);
+
+void BoxFilter(UBYTE* src, UBYTE* des, int width, int height, UBYTE* ker, int kw, int kh, UBYTE* temp);
+
 __global__ void gpuThresholdKernel(unsigned char* gpuOrig, unsigned char* gpuModif, int sizeArr, int threshold) {
 	//loop, compare threshold, change vals
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
@@ -35,6 +43,7 @@ __global__ void gpuThresholdKernel(unsigned char* gpuOrig, unsigned char* gpuMod
 unsigned char * gpuOriginalImage = nullptr;
 unsigned char * gpuModifiedImage = nullptr;
 Mat cpuOriginalImage;
+Mat cpuModifImage;
 int height;
 int width;
 int thresholdSlider = 195;
@@ -55,6 +64,7 @@ int main(int argc, char** argv) {
 	}
 	height = cpuOriginalImage.rows;
 	width = cpuOriginalImage.cols;
+
 	//int threshold = 14;
 
 	//declare vairables to put the data into
@@ -62,22 +72,22 @@ int main(int argc, char** argv) {
 	/*unsigned char * gpuOriginalImage;
 	unsigned char * gpuModifiedImage=0;
 	*/	
-	CpuThreshold(thresholdSlider, width, height, cpuOriginalImage.data);
-	cout << "CpuThreshold was a success" << endl << endl;
-	try {
-		//create space on the gpu to hold the modified and the image data
-		cudaError_t gpuStatus = mallocGPU(&gpuOriginalImage, &gpuModifiedImage, width, height);
-		if (gpuStatus != cudaSuccess) {
-			throw("Malloc GPU Failed");
-		}
-		cout << "gpuMalloc was a success" << endl << endl;
-		
-		//copy the cpu image data to the gpu
-		gpuStatus = copyToGPU(gpuOriginalImage, cpuOriginalImage.data, width, height);
-		if (gpuStatus != cudaSuccess) {
-			throw("Copy to GPU Failed");
-		}
-		cout << "cudaMemcpy cpuImg to gpuOrig Worked" << endl << endl;
+	//CpuThreshold(thresholdSlider, width, height, cpuOriginalImage.data);
+	//cout << "CpuThreshold was a success" << endl << endl;
+	//try {
+	//	//create space on the gpu to hold the modified and the image data
+	//	cudaError_t gpuStatus = mallocGPU(&gpuOriginalImage, &gpuModifiedImage, width, height);
+	//	if (gpuStatus != cudaSuccess) {
+	//		throw("Malloc GPU Failed");
+	//	}
+	//	cout << "gpuMalloc was a success" << endl << endl;
+	//	
+	//	//copy the cpu image data to the gpu
+	//	gpuStatus = copyToGPU(gpuOriginalImage, cpuOriginalImage.data, width, height);
+	//	if (gpuStatus != cudaSuccess) {
+	//		throw("Copy to GPU Failed");
+	//	}
+	//	cout << "cudaMemcpy cpuImg to gpuOrig Worked" << endl << endl;
 
 		//////update the image with the threshold
 		//int numBlocks = (1023 + width * height) / 1024;
@@ -98,19 +108,31 @@ int main(int argc, char** argv) {
 		//cout << "cudaMemcpu to Cpu Worked" << endl << endl;
 
 
-	}
+	/*}
 	catch (char* errMsg) {
 		cout << "Error: " << errMsg << endl;
 		cleanGPU(gpuOriginalImage, gpuModifiedImage);
 	}
+	*/ 
+
+	HighPrecisionTime hpt;
 	
+	UBYTE K[] = { 1,1,1,1,1,1,1,1,1 };
+	cpuModifImage = cpuOriginalImage.clone();
+	Mat cpuTemp = cpuOriginalImage.clone();
+
 	namedWindow("Display Window", WINDOW_NORMAL);
-	//imshow("Display Window", cpuOriginalImage);
-	createTrackbar("Slider", "Display Window", &thresholdSlider, 255, onTrack);
-	onTrack(thresholdSlider, 0);
+	imshow("Display Window", cpuOriginalImage);
+	waitKey(0);
+	hpt.TimeSinceLastCall();
+	BoxFilter(cpuOriginalImage.data, cpuModifImage.data, width, height, K, 3, 3, cpuTemp.data);
+	cout << "BoxFilter took " <<  hpt.TimeSinceLastCall() << " seconds" << endl;
+	imshow("Display Window", cpuModifImage);
+	//createTrackbar("Slider", "Display Window", &thresholdSlider, 255, onTrack);
+	//onTrack(thresholdSlider, 0);
 
 	waitKey(0);
-	cleanGPU(gpuOriginalImage, gpuModifiedImage);
+	//cleanGPU(gpuOriginalImage, gpuModifiedImage);
 	return 0;
 }
 void CpuThreshold(int threshold, int width, int height, unsigned char* data) {
@@ -234,4 +256,48 @@ cudaError_t copyToCPU(unsigned char* gpuData, unsigned char* cpuData, int width,
 	}
 	cout << "b"<< endl;
 	return copySuccess;
+}
+
+void BoxFilter(UBYTE* src, UBYTE* des, int width, int height, UBYTE* ker, int kw, int kh, UBYTE* temp) {
+
+	//this is bluring i thinhk, so yeah
+	int runSum = 0;
+	int kTimes = kw * kh;
+	// step through the entire image, every pixel
+	for (int sH = 1; sH < height -1; sH++) {
+		for (int sW = 1; sW < width-1; sW++) {
+			//cout << width << "(" << sH << "," << sW << ")" << endl;
+			//at each pixel grab the 8 surrounding pixel 
+			//if the pixel is in the middle of the image and has no restrictions
+				//top left
+			int pos = sH*width + sW;
+
+				runSum += src[pos - width - 1] * ker[0];
+				//top middle
+				runSum += src[pos - width ] * ker[1];
+				//top right
+				runSum += src[pos-width +1 ] * ker[2];
+				//immediate left
+				runSum += src[pos-1] * ker[3];
+				//pixPos
+				runSum += src[pos] * ker[4];
+				//immediate left
+				runSum += src[pos +1] * ker[5];
+				//bottom left
+				runSum += src[pos +width - 1]* ker[6];
+				//bottom middle
+				runSum += src[pos + width] * ker[7];
+				//bottom right
+				runSum += src[pos +width +1] *ker[8];
+
+				//add all the surrounding values and then divide my num values to get concentration of blur
+				//cout << sW *sH << "of " << width * height << endl;
+				des[pos] = int((float)runSum / (float)kTimes);
+				runSum = 0;
+		}
+	}
+
+
+
+
 }
